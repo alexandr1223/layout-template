52ef17330cdeeb205fb3beb224fd7103d7c072c5
alexandr1223 <pogrebnhyak19999@gmail.com>
2021-06-25 11:01:31 +0300

    Сборка v.2.0

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+
     $('img.img-svg').each(function(){
         var $img = $(this);
         var imgClass = $img.attr('class');
@@ -15,4 +16,46 @@ $(function () {
         $img.replaceWith($svg);
         }, 'xml');
     }); 
-})
+
+    function menu(menuBtn, block, close) {
+        if (document.querySelector(menuBtn)) {
+            document.querySelector(menuBtn).addEventListener('click', () => {
+                document.querySelector(block).style.cssText = 'top: 0';
+                document.body.style.overflow = "hidden"
+            })
+            document.querySelector(close).addEventListener('click', () => {
+                document.body.style.overflow = "auto"
+                document.querySelector(block).style.cssText = 'top: -150%';
+            })
+        }
+    } 
+    // menu('', '', '');
+
+    // Аккордеон
+    $(document).ready(function () {
+		if (document.querySelector('.accordeon')) {
+            $('.accordeon__open').click(function () {
+                $(this).toggleClass('ins').prev().slideToggle();
+                if ($(this).find('.accordeon__text').text().trim() == 'развернуть ответ') {
+                    $(this).find('.accordeon__text').text('cвернуть ответ')
+                } else {
+                    $(this).find('.accordeon__text').text('развернуть ответ')
+                }
+            });
+        }
+    });
+
+    // Кастомные селекты
+    // $('.calc__currency select').ddslick({
+    //     imagePosition: "left",
+    //     onSelected: function (data) {
+    //         console.log(data);
+    //     }
+    // });
+    // $('.calc__plan select').ddslick({
+    //     imagePosition: "left",
+    //     onSelected: function (data) {
+    //         console.log(data);
+    //     }
+    // });
+});
