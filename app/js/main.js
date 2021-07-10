c745327b14950d49b98d6ccbbd7caa87a185b087
alexandr1223 <pogrebnhyak19999@gmail.com>
2021-07-10 11:55:16 +0300

    origin master

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function () {
+document.addEventListener("DOMContentLoaded", function(event) {
 
     $('img.img-svg').each(function(){
         var $img = $(this);
@@ -20,42 +20,14 @@ $(function () {
     function menu(menuBtn, block, close) {
         if (document.querySelector(menuBtn)) {
             document.querySelector(menuBtn).addEventListener('click', () => {
-                document.querySelector(block).style.cssText = 'top: 0';
+                document.querySelector(block).style.cssText = 'left: 0';
                 document.body.style.overflow = "hidden"
             })
             document.querySelector(close).addEventListener('click', () => {
                 document.body.style.overflow = "auto"
-                document.querySelector(block).style.cssText = 'top: -150%';
+                document.querySelector(block).style.cssText = 'left: -100%';
             })
         }
     } 
     // menu('', '', '');
-
-    // Аккордеон
-    $(document).ready(function () {
-		if (document.querySelector('.accordeon')) {
-            $('.accordeon__open').click(function () {
-                $(this).toggleClass('ins').prev().slideToggle();
-                if ($(this).find('.accordeon__text').text().trim() == 'развернуть ответ') {
-                    $(this).find('.accordeon__text').text('cвернуть ответ')
-                } else {
-                    $(this).find('.accordeon__text').text('развернуть ответ')
-                }
-            });
-        }
-    });
-
-    // Кастомные селекты
-    // $('.calc__currency select').ddslick({
-    //     imagePosition: "left",
-    //     onSelected: function (data) {
-    //         console.log(data);
-    //     }
-    // });
-    // $('.calc__plan select').ddslick({
-    //     imagePosition: "left",
-    //     onSelected: function (data) {
-    //         console.log(data);
-    //     }
-    // });
 });
